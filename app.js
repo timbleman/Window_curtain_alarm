7445ba5d444c165f5c1444f2534cee338d24d75e
Lukas Rieger <lukas.rieger@stud.th-deg.de>
2021-05-20 21:52:00 +0200

    > refactoring, changing structure

--- app.js
+++ app.js
@@ -6,24 +6,38 @@ const path = require('path');
 var bodyParser = require('body-parser');
 
 const app = express(); 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT ||3000;
 
-app.use(express.static(path.join(__dirname, 'index')));
-app.use(bodyParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.get('/',express.static(path.join(__dirname, 'index')));
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname + '/index.html');
+// });
 
 
-app.get('/messages', function(req, res){
-    res.send(
-        
-    );
-});
+// app.get('/messages', function(req, res){
+//          myText = req.body.order;
+//          res.send('Your Text:' +myText);
+// });
 
 
+// app.get('/messages', function(req, res){ 
+//     var myText = req.body.mytext; //mytext is the name of your input box
+//     res.send('Your Text:' + myText); 
+// }); 
+
+
+// app.post('/myform', function(req, res) {
+//     var myText = req.body.mytext; //mytext is the name of your input box
+//       res.send('Your Text:' +myText); 
+//   });
+
+  app.post('/submit-form', (req, res) => {
+    const username = req.body.username
+    res.send('test')
+  })
 // app.post('/messages', function(req, res){
 //   var txt_folder_name = req.body.order;
 //   console.log("txt_folder_name", txt_folder_name)
@@ -33,10 +47,10 @@ app.get('/messages', function(req, res){
 // app.use('/',express.static(path.join(__dirname, '/')));
 
 const timeElapsed = Date.now();
-const today = new Date(timeElapsed); 
+const today = new Date(timeElapsed).getHours(); 
 
 // Console.log area
-console.log('Time: '.yellow, today.toUTCString());
+console.log('Time: '.yellow, today);
 console.log("Client Server started at http://localhost: ".green + port.toString()+ '');
 
 app.listen(port)
