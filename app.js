7578714f3140414adff014686db19fed3ab481a3
Lukas Rieger <lukas.rieger@stud.th-deg.de>
2021-05-20 17:27:49 +0200

    > added node_modules

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const path = require('path');
 const app = express(); 
 const port = process.env.PORT || 8080;
 
-app.use('/',express.static(path.join(__dirname, '/')));
+// app.use('/',express.static(path.join(__dirname, '/')));
 
 const timeElapsed = Date.now();
 const today = new Date(timeElapsed); 
 console.log('Time: '.yellow, today.toUTCString());
 
-app.use('/html',(req,res,next)=>{ 
-    req.sendFile(path.join(__dirname,'HTML','text.html'));
+app.use('/',(req,res,next)=>{ 
+    res.send('')
  });
  
  // sendFile will go here app.get('/', function(req, res) { res.send("everything works") });
@@ -22,3 +22,5 @@ app.use('/html',(req,res,next)=>{
 // sendFile(path.join(__dirname, '/index.html')) app.listen(port);
 
 console.log("Client Server started at http://localhost: ".green + port.toString()+ '');
+
+app.listen(port)
