8d599184dbe8baf392b7733853ad969038b03f9b
Lukas Rieger <lukas.rieger@stud.th-deg.de>
2021-05-20 17:51:52 +0200

    > added messages route > import javascript

--- app.js
+++ app.js
@@ -3,24 +3,38 @@
 var colors = require('colors'); 
 const express = require('express'); 
 const path = require('path');
+var bodyParser = require('body-parser');
 
 const app = express(); 
 const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'index')));
+app.use(bodyParser());
+
+app.get('/',express.static(path.join(__dirname, 'index')));
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+app.get('/messages', function(req, res){
+    res.send('tetst');
+});
+
+
+// app.post('/messages', function(req, res){
+//   var txt_folder_name = req.body.order;
+//   console.log("txt_folder_name", txt_folder_name)
+// })
+
 
 // app.use('/',express.static(path.join(__dirname, '/')));
 
 const timeElapsed = Date.now();
 const today = new Date(timeElapsed); 
+
+// Console.log area
 console.log('Time: '.yellow, today.toUTCString());
-
-app.use('/',(req,res,next)=>{ 
-    res.send('')
- });
- 
- // sendFile will go here app.get('/', function(req, res) { res.send("everything works") });
-
-// sendFile(path.join(__dirname, '/index.html')) app.listen(port);
-
 console.log("Client Server started at http://localhost: ".green + port.toString()+ '');
 
 app.listen(port)
